375dd803a6274bc78cd73b361d1aa439931f1d78
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-02-27 12:44:04 -0500

    Updated main view to use staus api

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,27 +6,16 @@ class MainController {
 
   constructor($http, $scope, socket) {
     this.$http = $http;
-    this.awesomeThings = [];
+    this.servers = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
+    $http.get('/api/servers/status').then(response => {
+      this.servers = response.data;
+      socket.syncUpdates('servers/status', this.servers);
     });
 
     $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('servers/status');
     });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
   }
 }
 
